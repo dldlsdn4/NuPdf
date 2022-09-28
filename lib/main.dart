d2fcec45ecb14a23894f262002ac476103624321
dldlsdn4 <dldlsdn4@naver.com>
2022-09-28 17:31:25 +0900

    20220928

--- lib/main.dart
+++ lib/main.dart
@@ -2,57 +2,69 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final sampleUrl = 'https://www.africau.edu/images/default/sample.pdf';
+
+  String? pdfFlePath;
+
+  Future<String> downloadAndSavePdf() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/chaket.pdf');
+    if (await file.exists()) {
+      return file.path;
+    }
+    final response = await http.get(Uri.parse(sampleUrl));
+    await file.writeAsBytes(response.bodyBytes);
+    return file.path;
+  }
+
+  void loadPdf() async {
+    pdfFlePath = await downloadAndSavePdf();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  _clickPdf() async {}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: PdfView(path: '/assets/chaket.pdf'),
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _clickPdf,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: Builder(builder: (context) {
+        return Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            title: Text('Plugin example app'),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text("Load pdf"),
+                  onPressed: loadPdf,
+                ),
+                if (pdfFlePath != null)
+                  Expanded(
+                    child: Container(
+                      child: PdfView(path: pdfFlePath!),
+                    ),
+                  )
+                else
+                  Text("Pdf is not Loaded"),
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
